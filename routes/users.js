a24913073188159aa8ad3dcfd0e2641a6375ef95
MuhammadUzairAbbasi <muhammaduzair25k@gmail.com>
2024-07-15 22:31:54 +0500

    user routes updated

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,34 @@
 import express from "express";
+import User from "../models/User.js";
 
 const Router = express.Router();
 
-Router.get("/", (req, res)=>{res.send("hey its user route")})
+Router.get("/", (req, res) => {
+  res.send("hey its user route");
+});
 
-export default Router
+Router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({ role: "Student" }); // Fetch only students
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+Router.delete("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    await User.deleteOne({ _id: id });
+    res.json({ message: "User deleted successfully" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+export default Router;
